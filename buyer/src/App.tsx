946bc571423862b2ebea05658a59e09682f7017a
Yuva <yuvapoojary@gmail.com>
2022-09-07 14:39:56 +0530

    gg

--- buyer/src/App.tsx
+++ buyer/src/App.tsx
@@ -73,7 +73,7 @@ const Navigation = () => {
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="buyer">
+      <BrowserRouter basename="/buyer">
         <AuthProvider>
           <Navigation />
         </AuthProvider>
